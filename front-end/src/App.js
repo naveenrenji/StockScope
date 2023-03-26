815cd3145b64e97447fe091e713de7cdd4b8717e
Naveen Mathews <52288469+naveenrenji@users.noreply.github.com>
2023-03-26 17:20:04 -0400

    Added Signup And Login Component with Validation

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Login from "./components/Authentication/Login";
+import Signup from "./components/Authentication/Signup";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          StockScope App under Development
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/naveenrenji/StockScope/tree/main"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          View on Github
-        </a>
-      </header>
+      <Routes>
+        <Route
+          index
+          element={
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>StockScope App under Development</p>
+              <a
+                className="App-link"
+                href="https://github.com/naveenrenji/StockScope/tree/main"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View on Github
+              </a>
+            </header>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }
