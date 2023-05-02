1e10404eedfea2c324e7ecc0c6e5105bdebc3d74
Naveen Mathews <52288469+naveenrenji@users.noreply.github.com>
2023-05-02 19:30:51 -0400

    Created the Backend api for Stock Charts

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,8 +1,10 @@
 //Here you will require route files and export the constructor method as shown in lecture code and worked in previous labs.
 const stockAPI = require("./stockAPI");
+const finnHubStockAPI = require('./finnHubStockAPI');
 
 const constructorMethod = (app) => {
   app.use("/stock", stockAPI);
+  app.use("/chart",finnHubStockAPI);
 
   app.use("*", (req, res) => {
     res.sendStatus(404);
