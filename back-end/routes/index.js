542bf554a583c36c5c311ff4d9317c6d6134948a
deepmanupy <reddymanudeep@gmail.com>
2023-05-03 12:04:06 -0400

    Trending Stocks; Backend API and frontend changes

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,10 +1,12 @@
 //Here you will require route files and export the constructor method as shown in lecture code and worked in previous labs.
 const stockAPI = require("./stockAPI");
 const finnHubStockAPI = require('./finnHubStockAPI');
+const yahooFinAPI = require('./yahooFinanceAPI')
 
 const constructorMethod = (app) => {
   app.use("/stock", stockAPI);
   app.use("/chart",finnHubStockAPI);
+  app.use("/screener", yahooFinAPI);
 
   app.use("*", (req, res) => {
     res.sendStatus(404);
