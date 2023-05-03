8f07ac7feb95d7ade5dfee6afd41413b0ac38a5a
Chaitanya Pawar <65788874+Chaitanyap22@users.noreply.github.com>
2023-05-02 20:07:45 -0400

    updated Authentication and chatbot UI

--- front-end/src/pages/Authentication/Signup.js
+++ front-end/src/pages/Authentication/Signup.js
@@ -4,9 +4,11 @@ import Form from "react-bootstrap/Form";
 import { Link, useNavigate } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import { doCreateUserWithEmailAndPassword } from "../../firebase/FirebaseFunctions";
-import { AuthContext } from "../../firebase/Auth";
+/* import { AuthContext } from "../../firebase/Auth"; */
 import SocialSignIn from "./SocialSignIn";
 import { checkName, checkPassword, checkEmail } from "../../helpers";
+import '../../assets/css/authentication.css';
+import { PersonCircle } from "react-bootstrap-icons";
 
 function Signup() {
   const [username, setUsername] = useState("");
@@ -56,7 +58,7 @@ function Signup() {
     if (validateInputs()) {
       try {
         await doCreateUserWithEmailAndPassword(email, password, username);
-        navigate("/login"); 
+        navigate("/login");
       } catch (error) {
         alert(error);
       }
@@ -64,10 +66,12 @@ function Signup() {
   };
 
   return (
-    <div className="d-flex justify-content-center mt-5">
-      <Card style={{ width: "20rem" }}>
-        <Card.Body>
-          <Card.Title>Signup</Card.Title>
+    <div className="authentication-body">
+      <div className="authentication-container">
+        <div className="authentication-container-wrapper">
+          <h3 className="authentication-login-text">
+            <PersonCircle /> Signup
+          </h3>
           <Form onSubmit={handleSubmit}>
             <Form.Group controlId="formBasicUsername">
               <Form.Label>Username</Form.Label>
@@ -123,18 +127,28 @@ function Signup() {
               </Form.Text>
             </Form.Group>
             <br></br>
-            <Button variant="primary" type="submit">
+            <button type="submit" className="authButton btn-semi-transparent">
               Signup
-            </Button>
+            </button>
 
           </Form>
-          <br></br>
-          <SocialSignIn />
-          <div>
-            <Link to="/login">Login with existing/created account</Link>
-          </div>{" "}
-        </Card.Body>
-      </Card>
+          <h4>
+            <span>Social Signup</span>
+          </h4>
+          <div className="authentication-social-media">
+            <SocialSignIn />
+            <a href="#">
+              <div className="icons8-facebook-circled authentication-social-mediaImg"></div>
+            </a>
+            <a href="#">
+              <div className="icons8-twitter authentication-social-mediaImg"></div>
+            </a>
+          </div>
+          <span className="authentication-ac">
+            Already have an Account? <Link to="/login">Login</Link>
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
