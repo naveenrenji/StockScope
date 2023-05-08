d15f1380ede19afcd9d937ea352f430bc7890237
Sagar <sagar.cse19@gmail.com>
2023-05-08 16:50:47 -0400

    env config for firebase

--- front-end/src/firebase/firebaseConfiguration.js
+++ front-end/src/firebase/firebaseConfiguration.js
@@ -1,16 +1,21 @@
 import { initializeApp } from "firebase/app";
-import { browserLocalPersistence, getAuth, GoogleAuthProvider, setPersistence } from "firebase/auth";
+import {
+  browserLocalPersistence,
+  getAuth,
+  GoogleAuthProvider,
+  setPersistence,
+} from "firebase/auth";
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-    apiKey: "AIzaSyBR3pn-0Ozmvyw_GQlEEFQhHiEW5vzLsIo",
-    authDomain: "stocksscope.firebaseapp.com",
-    projectId: "stocksscope",
-    storageBucket: "stocksscope.appspot.com",
-    messagingSenderId: "419796884417",
-    appId: "1:419796884417:web:ae053a4dc52fbf30652d40",
-    measurementId: "G-3ELQWJG66Y"
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
