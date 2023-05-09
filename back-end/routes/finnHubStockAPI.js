a5f38948794eb00e5b913294772d4c9f069adda1
Sagar <sagar.cse19@gmail.com>
2023-05-08 20:54:29 -0400

    finnHubAPI added

--- back-end/routes/finnHubStockAPI.js
+++ back-end/routes/finnHubStockAPI.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+
+const redis = require("redis");
+const client = redis.createClient();
+client.connect().then(() => {});
+
 require("dotenv").config();
 
 //cause dotenv isnt working for some reason.
@@ -12,12 +17,35 @@ router.route("/:symbol/:resolution/:from/:to").get(async (req, res) => {
 
   try {
     let data = await axios.get(url);
-    res.status(200).json(data.data);
+    await client.set(`company-overview:${stockName}`, stringData);
+    await client.expire(`company-overview:${stockName}`, 100);
+    return res.status(200).json(data.data);
   } catch (error) {
     return res.status(error.statusCode).json({
       error: error.message,
     });
   }
+});
+
+router.route("/:symbol").get(async (req, res) => {
+  try {
+    let symbol = req.params.symbol;
+    let temp = await client.get(`stock-symbol:${symbol}`);
+    if (temp) {
+      let data = JSON.parse(temp);
+      return res.status(200).json(data);
+    }
+    let url = `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${finnhubApiKey}`;
+    let { data } = await axios.get(url);
+    console.log(data);
+    await client.set(`stock-symbol:${symbol}`, JSON.stringify(data));
+    await client.expire(`stock-symbol:${symbol}`, 72000);
+    return res.status(200).json(data);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ error: e });
+  }
+  // https://finnhub.io/api/v1/quote?symbol=AAPL&token=cgvbft1r01qqk0dog72gcgvbft1r01qqk0dog730
 });
 
 module.exports = router;
