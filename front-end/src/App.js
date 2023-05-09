dc8eebf7569d0f6e608ecf09a182fb87b6ee0455
Naveen Mathews <52288469+naveenrenji@users.noreply.github.com>
2023-05-08 20:07:28 -0400

    Agent restrictions

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,39 +1,88 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import MarketNews from './pages/MarketNews';
-import Stocks from './pages/Stocks';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+import MarketNews from "./pages/MarketNews";
+import Stocks from "./pages/Stocks";
 // import Profile from './pages/Profile';
-import Signup from './pages/Authentication/Signup';
-import Login from './pages/Authentication/Login';
-import PortfolioBrief from './components/Portfolio/PortfolioBrief';
-import StockSummary from './components/Stock/Summary';
-import StockNews from './components/Stock/News';
-import NotFoundPage from './pages/404';
-import HistoricalData from './components/Stock/HistoricalData';
-import Agent from './components/Agent/Agent';
+import Signup from "./pages/Authentication/Signup";
+import Login from "./pages/Authentication/Login";
+import PortfolioBrief from "./components/Portfolio/PortfolioBrief";
+import StockSummary from "./components/Stock/Summary";
+import StockNews from "./components/Stock/News";
+import NotFoundPage from "./pages/404";
+import HistoricalData from "./components/Stock/HistoricalData";
+import Agent from "./components/Agent/Agent";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase/firebaseConfiguration";
+
+function AuthenticationHandler() {
+  const [user, setUser] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log(user);
+        const uid = user.uid;
+        console.log("uid", user);
+        setUser(true);
+      } else {
+        navigate("/");
+      }
+    });
+    return unsubscribe;
+  }, [navigate]);
+
+  return user ? (
+        <Routes>
+          <Route key="agent" path="/agent" element={<Agent />} />
+          <Route key="signup" path="/signup" element={<Signup />} />
+          <Route key="login" path="/login" element={<Login />} />
+          <Route path="*" element={<Agent />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route key="home" path="/" element={<Home />} />
+          <Route key="market-news" path="/market-news" element={<MarketNews />} />
+          <Route key="portfolio" path="/portfolio" element={<Stocks />} />
+          {/* <Route key="profile" path="/profile" element={<Profile />} /> */}
+          <Route key="signup" path="/signup" element={<Signup />} />
+          <Route key="login" path="/login" element={<Login />} />
+          <Route
+            key="portfolio_brief"
+            path="/portfolio/:name"
+            element={<PortfolioBrief />}
+          />
+          <Route
+            key="stock_summary"
+            path="/stock/summary"
+            element={<StockSummary symbol="TSLA" name="Tesla, Inc" />}
+          />
+          <Route
+            key="stock_news"
+            path="/stock/news"
+            element={<StockNews symbol="TSLA" name="Tesla, Inc" />}
+          />
+          <Route
+            key="stock_historical_data"
+            path="/stock/historicaldata"
+            element={<HistoricalData symbol="TSLA" name="Tesla, Inc" />}
+          />
+          <Route key="agent" path="/agent" element={<Agent />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      )
+};
 
 function App() {
   return (
-
     <Router>
-      <Routes>
-        <Route key="home" path="/" element={<Home />} />
-        <Route key="market-news" path="/market-news" element={<MarketNews />} />
-        <Route key="portfolio" path="/portfolio" element={<Stocks />} />
-        {/* <Route key="profile" path="/profile" element={<Profile />} /> */}
-        <Route key="signup" path="/signup" element={<Signup />} />
-        <Route key="login" path="/login" element={<Login />} />
-        <Route key="portfolio_brief" path="/portfolio/:name" element={<PortfolioBrief />} />
-        <Route key="stock_summary" path="/stock/summary" element={<StockSummary symbol="TSLA" name="Tesla, Inc" />} />
-        <Route key="stock_news" path="/stock/news" element={<StockNews symbol="TSLA" name="Tesla, Inc" />} />
-        <Route key="stock_historical_data" path="/stock/historicaldata" element={<HistoricalData symbol="TSLA" name="Tesla, Inc" />} />
-        <Route key="agent" path="/agent" element={<Agent />} />
-
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <AuthenticationHandler />
     </Router>
   );
 }
 
+
+
 export default App;
+
